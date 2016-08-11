fada6dc7b6e51ad2031c3f7e2799395666879007
TsT <tst2005@gmail.com>
2016-08-11 20:09:09 +0200

    really improved

--- ansicolors.sh
+++ ansicolors.sh
@@ -1,140 +1,102 @@
 #!/bin/sh
 
 ansicolors() {
+	local noerror=false
 	local printf=printf
+	local fmt=''
 	while [ $# -gt 0 ]; do
-
 		case "$1" in
-		('%{raw}')		printf='printf %s'	;;
-		('%{eval}')		printf='printf'		;;
-
-		('%{NONE}')		printf ''		;;
-		('%{BLACK}')		$printf '\033[0;30m'	;;
-		('%{RED}')		$printf '\033[0;31m'	;;
-		('%{GREEN}')		$printf '\033[0;32m'	;;
-		('%{BROWN}')		$printf '\033[0;33m'	;;
-		('%{BLUE}')		$printf '\033[0;34m'	;;
-		('%{PURPLE}')		$printf '\033[0;35m'	;;
-		('%{CYAN}')		$printf '\033[0;36m'	;;
-		('%{LIGHT_GREY}')	$printf '\033[0;37m'	;;
-
-		('%{DARK_GREY}')	$printf '\033[1;30m'	;;
-		('%{LIGHT_RED}')	$printf '\033[1;31m'	;;
-
-		('%{LIGHT_GREEN}')	$printf '\033[32m'	;;
-		('%{YELLOW}')		$printf '\033[33m'	;;
-
-		('%{LIGHT_GREY_BG_YELLOW}')	$printf '\033[43;30m'	;;
-
-		('%{LIGHT_BLUE}')	$printf '\033[34m'	;;
-		('%{LIGHT_PURPLE}')	$printf '\033[35m'	;;
-		('%{LIGHT_CYAN}')	$printf '\033[36m'	;;
-		('%{WHITE}')		$printf '\033[37m'	;;
-		('%{DEFAULT}')		$printf '\033[0m'	;;
-
-	# Reset
-		('%{Color_Off}')	$printf '\033[0m'	;;
-
-	# Regular Colors = 0
-		('%{Black}')		$printf '\033[0;30m'	;;
-		('%{Red}')		$printf '\033[0;31m'	;;
-		('%{Green}')		$printf '\033[0;32m'	;;
-		('%{Yellow}')		$printf '\033[0;33m'	;;
-		('%{Blue}')		$printf '\033[0;34m'	;;
-		('%{Purple}')		$printf '\033[0;35m'	;;
-		('%{Cyan}')		$printf '\033[0;36m'	;;
-		('%{White}')		$printf '\033[0;37m'	;;
-
-	# Bold = 1
-		('%{B_Black}')	$printf '\033[1;30m'	;;
-		('%{B_Red}')	$printf '\033[1;31m'	;;
-		('%{B_Green}')	$printf '\033[1;32m'	;;
-		('%{B_Yellow}')	$printf '\033[1;33m'	;;
-		('%{B_Blue}')	$printf '\033[1;34m'	;;
-		('%{B_Purple}')	$printf '\033[1;35m'	;;
-		('%{B_Cyan}')	$printf '\033[1;36m'	;;
-		('%{B_White}')	$printf '\033[1;37m'	;;
-
-	# Underline = 4
-		('%{U_Black}')	$printf '\033[4;30m'	;;
-		('%{U_Red}')	$printf '\033[4;31m'	;;
-		('%{U_Green}')	$printf '\033[4;32m'	;;
-		('%{U_Yellow}')	$printf '\033[4;33m'	;;
-
-		('%{u_yellow}') $printf '\033[33;4m'    ;;
-
-		('%{U_Blue}')	$printf '\033[4;34m'	;;
-		('%{U_Purple}')	$printf '\033[4;35m'	;;
-		('%{U_Cyan}')	$printf '\033[4;36m'	;;
-		('%{U_White}')	$printf '\033[4;37m'	;;
-
-	# Background = color+10
-		('%{BG_Black}')	$printf '\033[40m'	;;
-		('%{BG_Red}')	$printf '\033[41m'	;;
-		('%{BG_Green}')	$printf '\033[42m'	;;
-		('%{BG_Yellow}')	$printf '\033[43m'	;;
-		('%{BG_Blue}')	$printf '\033[44m'	;;
-		('%{BG_Purple}')	$printf '\033[45m'	;;
-		('%{BG_Cyan}')	$printf '\033[46m'	;;
-		('%{BG_White}')	$printf '\033[47m'	;;
-
-	# Background Bold
-		('%{BG_B_Black}')	$printf '\033[1;40m'	;;
-		('%{BG_B_Red}')	$printf '\033[1;41m'	;;
-		('%{BG_B_Green}')	$printf '\033[1;1;42m'	;;
-		('%{BG_B_Yellow}')	$printf '\033[1;43m'	;;
-		('%{BG_B_Blue}')	$printf '\033[1;44m'	;;
-		('%{BG_B_Purple}')	$printf '\033[1;45m'	;;
-		('%{BG_B_Cyan}')	$printf '\033[1;46m'	;;
-		('%{BG_B_White}')	$printf '\033[1;47m'	;;
-
-	# High Intensity = color+60
-		('%{HI_Black}')	$printf '\033[0;90m'	;;
-		('%{HI_Red}')	$printf '\033[0;91m'	;;
-		('%{HI_Green}')	$printf '\033[0;92m'	;;
-		('%{HI_Yellow}')	$printf '\033[0;93m'	;;
-		('%{HI_Blue}')	$printf '\033[0;94m'	;;
-		('%{HI_Purple}')	$printf '\033[0;95m'	;;
-		('%{HI_Cyan}')	$printf '\033[0;96m'	;;
-		('%{HI_White}')	$printf '\033[0;97m'	;;
-
-	# Bold High Intensity
-		('%{HI_B_Black}')	$printf '\033[1;90m'	;;
-		('%{HI_B_Red}')	$printf '\033[1;91m'	;;
-		('%{HI_B_Green}')	$printf '\033[1;92m'	;;
-		('%{HI_B_Yellow}')	$printf '\033[1;93m'	;;
-		('%{HI_B_Blue}')	$printf '\033[1;94m'	;;
-		('%{HI_B_Purple}')	$printf '\033[1;95m'	;;
-		('%{HI_B_Cyan}')	$printf '\033[1;96m'	;;
-		('%{HI_B_White}')	$printf '\033[1;97m'	;;
-
-	# High Intensity backgrounds = bg+60
-		('%{HI_BG_Black}')	$printf '\033[0;100m'	;;
-		('%{HI_BG_Red}')	$printf '\033[0;101m'	;;
-		('%{HI_BG_Green}')	$printf '\033[0;102m'	;;
-		('%{HI_BG_Yellow}')	$printf '\033[0;103m'	;;
-		('%{HI_BG_Blue}')	$printf '\033[0;104m'	;;
-		('%{HI_BG_Purple}')	$printf '\033[0;105m'	;;
-		('%{HI_BG_Cyan}')	$printf '\033[0;106m'	;;
-		('%{HI_BG_White}')	$printf '\033[0;107m'	;;
-
-	# High Intensity Bold backgrounds
-		('%{HI_BG_B_Black}')	$printf '\033[1;100m'	;;
-		('%{HI_BG_B_Red}')	$printf '\033[1;101m'	;;
-		('%{HI_BG_B_Green}')	$printf '\033[1;102m'	;;
-		('%{HI_BG_B_Yellow}')	$printf '\033[1;103m'	;;
-		('%{HI_BG_B_Blue}')	$printf '\033[1;104m'	;;
-		('%{HI_BG_B_Purple}')	$printf '\033[1;105m'	;;
-		('%{HI_BG_B_Cyan}')	$printf '\033[1;106m'	;;
-		('%{HI_BG_B_White}')	$printf '\033[1;107m'	;;
-
-		('%{LF}')		$printf '\n'	;;
-		('%{CRLF}')		$printf '\r\n'	;;
-		('%{CR}')		$printf '\r'	;;
-
-		('%{'*'}')		echo >&2 "ERROR: syntax error: $1" ; return 1 ;;
-		(*)		printf '%s' "$1"	;;
+			('%{'*'}') ;;
+			(*) printf '%s' "$1"; shift; continue ;;
 		esac
+
+		local list="${1#%{}"; list="${list%\}}"		# Remove the prefix '%{' and suffix '}'
+		local codes=''
+		local trailing=''
+		local bg=false hi=false				# Background and High Intensity
+		local fgc=''					# Foreground color code
+		local bgc=''					# Background color code
+		local s0='' s1='' s2='' s3='' s4='' s5='' s6=''	# all Styles
+			
+		IFS="${IFS}+_"					# Split the list using spaces or '+' separators
+		for x in $list; do
+			local c=''				# tmp color code
+			case "$x" in
+				('raw')		fmt='%s' ;;
+				('eval')	fmt=''   ;;
+
+				('color=no'|'color=never')	printf=':'	;;
+				('color=yes'|'color=always')	printf='printf' ;;
+				('color=auto'|'color')
+					[ -t 1 ] && printf='printf' || printf=':'
+				;;
+
+				('noerror')		noerror=true ;;
+				('error')		noerror=false ;;
+
+				('bg')			bg=true ;;
+				('hi')			hi=true ;;
+
+				('black')		c=30	;;
+				('red')			c=31	;;
+				('green')		c=32	;;
+				('yellow')		c=33	;;
+				('blue')		c=34	;;
+				('purple')		c=35	;;
+				('cyan')		c=36	;;
+				('white')		c=37	;;
+
+			#	('none')		exit 1		;;
+			#	('reset')		codes='0' ; break ;;
+				('-'|''|'normal')	s0=true		;;
+				('default')		s0=true		;;
+				('b'|'bold')		s1=true		;;
+				('s1')			s1=true		;;
+				('s2')			s2=true		;;
+				('s3')			s3=true		;;
+				('u'|'underline')	s4=true	;;
+				('s4')			s4=true		;;
+				('s5')			s5=true		;;
+				('s6')			s6=true		;;
+				('lf'|'\n')		trailing='\n'	;;
+				('cr'|'\r')		trailing='\r'	;;
+				('crlf'|'\r\n')		trailing='\r\n' ;;
+				(*)
+					if ${noerror:-false}; then
+						continue
+					fi
+					echo >&2 "ERROR: instruction not supported: $x"
+					return 1
+				;;
+			esac
+			if [ -n "$c" ]; then
+				# Regular                   = color       [30-37]
+				# Background                = color+10    [40-47]
+				# High Intensity            = color+60    [90-97]
+				# Background High Intensity = color+60+10 [100-107]
+				if ${hi}; then
+					c=$(( $c + 60 ))
+				fi
+				if $bg; then
+					bgc="$(( $c + 10 ))"
+				else
+					fgc="$c"
+				fi
+				bg=false
+				hi=false
+				c=''
+			fi
+		done
+		if [ -z "$codes" ]; then # if codes exists use it as is (useful to force a returned code value)
+			codes="${s0:+;0}${s1:+;1}${s2:+;2}${s3:+;3}${s4:+;4}${s5:+;5}${s6:+;6}${fgc:+;$fgc}${bgc:+;$bgc}" # got something like ';0;1;4;37;43'
+			codes="${codes#;}" # remove the first ';' and got '0;1;4;37;43'
+		fi
+		if [ -n "$codes" ]; then
+			$printf $fmt '\033['"$codes"'m'
+		fi
+		if [ -n "$trailing" ]; then
+			$printf $fmt "$trailing"
+		fi
 		shift
 	done
 }
--- test.sh
+++ test.sh
@@ -4,6 +4,18 @@ ansicolors() {
 	./ansicolors.sh "$@"
 }
 
+#ansicolors %{raw} '%{red bold}' ;echo
+#ansicolors %{color=yes} %{hi+red+bold} hello %{default} ' ' %{_+yellow} world %{LF}
+#ansicolors %{u+red} toto1234 ;echo
+#echo '               '
+#ansicolors %{regular+red} toto1234 ;echo
+
+ansicolors '%{s1+s2+bg+hi+red}' hello '%{}' ' ' %{blue} world %{eval} %{LF}
+echo
+
+
+exit 0
+
 echo	ansicolors "%{RED}" "hello" "%{DEFAULT}" ' ' "%{BG_Blue}" wor "%{HI_BG_Blue}" ld "%{DEFAULT}" %{LF}
 	ansicolors "%{RED}" "hello" "%{DEFAULT}" ' ' "%{BG_Blue}" wor "%{HI_BG_Blue}" ld "%{DEFAULT}" %{LF}
 
@@ -11,3 +23,6 @@ echo	ansicolors "%{RED}" "hello" "%{DEFAULT}" ' ' "%{BG_Blue}" wor "%{HI_BG_Blue
 #	printf "$(ansicolors %{raw} %{RED} hello %{DEFAULT} ' ' %{BG_Blue} wor %{HI_BG_Blue} ld %{DEFAULT} %{LF})"
 
 ansicolors %{U_Yellow} U_Yellow %{DEFAULT} ' ' %{u_yellow} u_yellow %{DEFAULT} %{LF}
+
+ansicolors %{color=no} %{U_Yellow} U_Yellow %{DEFAULT} ' ' %{u_yellow} u_yellow %{DEFAULT} %{LF}
+
